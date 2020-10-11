6e560de02c74078c46a1e73323d6e3f3dd3d83d2
Henri Jian <henrijian@gmail.com>
2020-10-11 23:06:41 +0800

    project 1b second commit.

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,7 +1,9 @@
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
-        int diff = Character.compare(x, y);
+        char lowerX = Character.toLowerCase(x);
+        char lowerY = Character.toLowerCase(y);
+        int diff = Character.compare(lowerX, lowerY);
         int absDiff = java.lang.Math.abs(diff);
         return absDiff == 1;
     }
