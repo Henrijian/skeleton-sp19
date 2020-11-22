06c63821d11b8163295fe6c566e3c0eb0691a64d
Henri Jian <henrijian@gmail.com>
2020-11-22 17:49:12 +0800

    hw1 - 2018 - finished.

---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,28 @@
+package synthesizer;
+
+public interface BoundedQueue<T> extends Iterable<T> {
+    /* return size of the buffer. */
+    int capacity();
+
+    /* return number of items currently in the buffer. */
+    int fillCount();
+
+    /* add item x to the end of the buffer. */
+    void enqueue(T x);
+
+    /* delete and return item x from the front of the buffer. */
+    T dequeue();
+
+    /* return item from the front of the buffer. (but do not delete) */
+    T peek();
+
+    /* is the buffer is empty? */
+    default boolean isEmpty() {
+        return fillCount() == 0;
+    }
+
+    /* is the buffer is full? */
+    default boolean isFull() {
+        return fillCount() == capacity();
+    }
+}
