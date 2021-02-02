840befb722cfd7353415381c1062f300da7e4bb2
Henri Jian <henrijian@gmail.com>
2021-02-02 22:03:23 +0800

    add tessellated hexagon to world.

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -5,127 +5,302 @@ import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
 
 import java.awt.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
-    private static final int WIDTH = 60;
+    private static final int WIDTH = 28;
     private static final int HEIGHT = 30;
+    private static final int SIZE = 3;
 
-    private static void validateSide(int side) {
-        if (side <= 0) {
-            throw new IllegalArgumentException("side must be positive value, getting side: " + side);
-        }
-    }
+    static class Hexagon {
+        /**
+         * @lowerLeftPt lower left point of boundary rectangle of hexagon.
+         * @size side length of hexagon.
+         * */
+        public final Point lowerLeftPt;
+        public final int size;
 
-    private static void validateLevel(int level) {
-        if (level < 0) {
-            throw new IllegalArgumentException("level must be positive value, getting level: " + level);
-        }
-    }
-
-    private static void validateSideAndLevel(int side, int level) {
-        validateSide(side);
-        validateLevel(level);
-        int hexHeight = hexagonHeight(side);
-        if (hexHeight <= level) {
-            throw new IllegalArgumentException("level is out of hexagon height, level: " + level + ", height: " + hexHeight);
-        }
-    }
-
-    /**
-     * Get height of hexagon with size side.
-     * @ size, side size of hexagon.
-     * */
-    public static int hexagonHeight(int size) {
-        validateSide(size);
-        return size * 2;
-    }
-
-    /**
-     * Get width of hexagon with size side.
-     * @ size, side size of hexagon.
-     */
-    public static int hexagonWidth(int size) {
-        validateSide(size);
-        return 3 * size - 2;
-    }
-
-    /**
-     * Get start X position of hexagon, which lower left point is (x, y), at level level(0 - base).
-     */
-    public static int hexagonStartX(Point lowerLeftPt, int size, int level) {
-        validateSideAndLevel(size, level);
-        int startX = lowerLeftPt.x;
-        int hexHeight = hexagonHeight(size);
-        int hexHalfHeight = hexHeight / 2;
-        int deltaX;
-        if (level < hexHalfHeight) {
-            deltaX = size - 1 - level;
-        } else {
-            deltaX = level - size;
-        }
-        return startX + deltaX;
-    }
-
-    /**
-     * Get end position of hexagon, which lower left point is (x, y), at level level(0 - base).
-     */
-    public static int hexagonEndX(Point lowerLeftPt, int size, int level) {
-        validateSideAndLevel(size, level);
-        int startX = lowerLeftPt.x;
-        int hexWidth = hexagonWidth(size);
-        int hexHeight = hexagonHeight(size);
-        int hexHalfHeight = hexHeight / 2;
-        int deltaX;
-        if (level < hexHalfHeight) {
-            deltaX = hexWidth - 1 - size + 1 + level;
-        } else {
-            deltaX = hexWidth - 1  - level + size;
-        }
-        return startX + deltaX;
-    }
-
-    /**
-     * Add hexagon at position lowerLeftPt which is lower left point of rectangle of added hexagon,
-     * then use tile to fill this added hexagon.
-     * */
-    public static void addHexagon(TETile[][] world, Point lowerLeftPt, int size, TETile tile) {
-        // Check arguments.
-        if (world == null) {
-            throw new IllegalArgumentException("world cannot be null.");
-        }
-        validateSide(size);
-        if (tile == null) {
-            throw new IllegalArgumentException("tile for constructing hexagon cannot be null.");
-        }
-
-        int worldWidth = world.length;
-        int worldHeight = world[0].length;
-        int hexHeight = hexagonHeight(size);
-        for (int level = 0; level < hexHeight; level++) {
-            int hexY = lowerLeftPt.y + level;
-            if (hexY < 0 || worldHeight <= hexHeight) {
-                continue;
+        public Hexagon(Point lowerLeftPt, int size) {
+            if (lowerLeftPt == null) {
+                throw new IllegalArgumentException("Initiating Hexagon with null lowerLeftPt is invalid.");
             }
-            int hexStartX = hexagonStartX(lowerLeftPt, size, level);
-            int hexEndX = hexagonEndX(lowerLeftPt, size, level);
-            for (int hexX = hexStartX; hexX <= hexEndX; hexX++) {
-                if (hexX < 0 || worldWidth <= hexX) {
+            if (size <= 0) {
+                throw new IllegalArgumentException("Initiating Hexagon with not positive size is invalid, size: " + size);
+            }
+            this.lowerLeftPt = new Point(lowerLeftPt.x, lowerLeftPt.y);
+            this.size = size;
+        }
+
+        /**
+         * Get height of hexagon.
+         * */
+        public int height() {
+            return size * 2;
+        }
+
+        /**
+         * Get width of hexagon.
+         */
+        public int width() {
+            return 3 * size - 2;
+        }
+
+        /**
+         * Get point of top left corner of hexagon.
+         */
+        public Point topLeftPt() {
+            int level = height() - 1;
+            int y = lowerLeftPt.y + level;
+            int x = vertStartX(level);
+            return new Point(x, y);
+        }
+
+        /**
+         * Get point of top right corner of hexagon.
+         */
+        public Point topRightPt() {
+            int level = height() - 1;
+            int y = lowerLeftPt.y + level;
+            int x = vertEndX(level);
+            return new Point(x, y);
+        }
+
+        /**
+         * Get point of right top corner of hexagon.
+         */
+        public Point rightTopPt() {
+            int level = height() / 2;
+            int y = lowerLeftPt.y + level;
+            int x = vertEndX(level);
+            return new Point(x, y);
+        }
+
+        /**
+         * Get point of right bottom corner of hexagon.
+         */
+        public Point rightBottomPt() {
+            int level = height() / 2 - 1;
+            int y = lowerLeftPt.y + level;
+            int x = vertEndX(level);
+            return new Point(x, y);
+        }
+
+        /**
+         * Get point of bottom right corner of hexagon.
+         */
+        public Point bottomRightPt() {
+            int level = 0;
+            int y = lowerLeftPt.y + level;
+            int x = vertEndX(level);
+            return new Point(x, y);
+        }
+
+        /**
+         * Get point of bottom left corner of hexagon.
+         */
+        public Point bottomLeftPt() {
+            int level = 0;
+            int y = lowerLeftPt.y + level;
+            int x = vertStartX(level);
+            return new Point(x, y);
+        }
+
+        /**
+         * Get point of left bottom corner of hexagon.
+         */
+        public Point leftBottomPt() {
+            int level = height() / 2 - 1;
+            int y = lowerLeftPt.y + level;
+            int x = vertStartX(level);
+            return new Point(x, y);
+        }
+
+        /**
+         * Get point of left top corner of hexagon.
+         */
+        public Point leftTopPt() {
+            int level = height() / 2;
+            int y = lowerLeftPt.y + level;
+            int x = vertStartX(level);
+            return new Point(x, y);
+        }
+
+        /**
+         * Get the top tessellated hexagon.
+         */
+        public Hexagon tessellatedTopHexagon() {
+            int tessellatedX = leftTopPt().x;
+            int tessellatedY = topLeftPt().y + 1;
+            Point tessellatedPt = new Point(tessellatedX, tessellatedY);
+            return new Hexagon(tessellatedPt, size);
+        }
+
+        /**
+         * Get the top right tessellated hexagon.
+         */
+        public Hexagon tessellatedTopRightHexagon() {
+            int tessellatedX = topRightPt().x + 1;
+            int tessellatedY = rightTopPt().y;
+            Point tessellatedPt = new Point(tessellatedX, tessellatedY);
+            return new Hexagon(tessellatedPt, size);
+        }
+
+        /**
+         * Get the bottom right tessellated hexagon.
+         */
+        public Hexagon tessellatedBottomRightHexagon() {
+            int tessellatedX = bottomRightPt().x + 1;
+            int tessellatedY = bottomRightPt().y - height() / 2;
+            Point tessellatedPt = new Point(tessellatedX, tessellatedY);
+            return new Hexagon(tessellatedPt, size);
+        }
+
+        /**
+         * Get the bottom tessellated hexagon.
+         */
+        public Hexagon tessellatedBottomHexagon() {
+            int tessellatedX = leftBottomPt().x;
+            int tessellatedY = bottomRightPt().y - height();
+            Point tessellatedPt = new Point(tessellatedX, tessellatedY);
+            return new Hexagon(tessellatedPt, size);
+        }
+
+        /**
+         * Get the bottom left tessellated hexagon.
+         */
+        public Hexagon tessellatedBottomLeftHexagon() {
+            int tessellatedX = bottomLeftPt().x - width();
+            int tessellatedY = bottomLeftPt().y - height() / 2;
+            Point tessellatedPt = new Point(tessellatedX, tessellatedY);
+            return new Hexagon(tessellatedPt, size);
+        }
+
+        /**
+         * Get the bottom left tessellated hexagon.
+         */
+        public Hexagon tessellatedTopLeftHexagon() {
+            int tessellatedX = topLeftPt().x - width();
+            int tessellatedY = leftTopPt().y;
+            Point tessellatedPt = new Point(tessellatedX, tessellatedY);
+            return new Hexagon(tessellatedPt, size);
+        }
+
+        private void validateLevel(int level) {
+            int hexHeight = height();
+            if (level < 0 || hexHeight <= level) {
+                throw new IllegalArgumentException("level must be within height, level: " + level + ", height: " + hexHeight);
+            }
+        }
+
+        /**
+         * Get start X position of hexagon at level level(0 - base).
+         */
+        public int vertStartX(int level) {
+            validateLevel(level);
+            int startX = lowerLeftPt.x;
+            int hexHeight = height();
+            int hexHalfHeight = hexHeight / 2;
+            int deltaX;
+            if (level < hexHalfHeight) {
+                deltaX = size - 1 - level;
+            } else {
+                deltaX = level - size;
+            }
+            return startX + deltaX;
+        }
+
+        /**
+         * Get end X position of hexagon at level level(0 - base).
+         */
+        public int vertEndX(int level) {
+            validateLevel(level);
+            int startX = lowerLeftPt.x;
+            int hexWidth = width();
+            int hexHeight = height();
+            int hexHalfHeight = hexHeight / 2;
+            int deltaX;
+            if (level < hexHalfHeight) {
+                deltaX = hexWidth - 1 - size + 1 + level;
+            } else {
+                deltaX = hexWidth - 1  - level + size;
+            }
+            return startX + deltaX;
+        }
+
+        /**
+         * Add hexagon to world with tile to fill in hexagon.
+         * */
+        public void addToWorld(TETile[][] world, TETile tile) {
+            // Check arguments.
+            if (world == null) {
+                throw new IllegalArgumentException("world for addToWorld cannot be null.");
+            }
+            if (tile == null) {
+                throw new IllegalArgumentException("tile for addToWorld hexagon cannot be null.");
+            }
+            int worldWidth = world.length;
+            int worldHeight = world[0].length;
+            int hexHeight = height();
+            for (int level = 0; level < hexHeight; level++) {
+                int hexY = lowerLeftPt.y + level;
+                if (hexY < 0 || worldHeight <= hexHeight) {
                     continue;
                 }
-                world[hexX][hexY] = tile;
+                int hexStartX = vertStartX(level);
+                int hexEndX = vertEndX(level);
+                for (int hexX = hexStartX; hexX <= hexEndX; hexX++) {
+                    if (hexX < 0 || worldWidth <= hexX) {
+                        continue;
+                    }
+                    world[hexX][hexY] = tile;
+                }
             }
         }
+
+        public boolean inWorld(TETile[][] world) {
+            // Check arguments.
+            if (world == null) {
+                throw new IllegalArgumentException("world for inWorld cannot be null.");
+            }
+            int worldWidth = world.length;
+            int worldHeight = world[0].length;
+            int minX = lowerLeftPt.x;
+            int maxX = lowerLeftPt.x + width() - 1;
+            int minY = lowerLeftPt.y;
+            int maxY = lowerLeftPt.y + height() - 1;
+            return (0 <= minX && maxX < worldWidth && 0 <= minY && maxY <= worldHeight);
+        }
+    }
+
+    /** Picks a RANDOM tile with a 33% change of being
+     *  a wall, 33% chance of being a flower, and 33%
+     *  chance of being empty space.
+     */
+    private static TETile randomTile() {
+        Random random = new Random();
+        int tileNum = random.nextInt(6);
+        return switch (tileNum) {
+            case 0 -> Tileset.WALL;
+            case 1 -> Tileset.FLOWER;
+            case 2 -> Tileset.GRASS;
+            case 3 -> Tileset.WATER;
+            case 4 -> Tileset.SAND;
+            case 5 -> Tileset.TREE;
+            default -> Tileset.NOTHING;
+        };
     }
 
     public static void main(String[] args) {
-        // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
+        // Initialize the tile rendering engine with a window of size WIDTH x HEIGHT
         TERenderer renderer = new TERenderer();
         renderer.initialize(WIDTH, HEIGHT);
 
-        // initialize tiles
+        // Initialize tiles
         TETile[][] world = new TETile[WIDTH][HEIGHT];
         for (int x = 0; x < WIDTH; x += 1) {
             for (int y = 0; y < HEIGHT; y += 1) {
@@ -133,7 +308,27 @@ public class HexWorld {
             }
         }
 
-        addHexagon(world, new Point(0, 0), 10, Tileset.FLOWER);
+        // Find first hexagon position.
+        Hexagon hexagon = new Hexagon(new Point(0,0), SIZE);
+        Point lowerLeftPt = new Point(WIDTH / 2 - hexagon.width() / 2, 0);
+
+        // Add hexagon to world.
+        Queue<Hexagon> hexQueue = new LinkedList<>();
+        hexQueue.add(new Hexagon(lowerLeftPt, SIZE));
+        while (hexQueue.size() > 0) {
+            hexagon = hexQueue.poll();
+            Hexagon leftHexagon = hexagon.tessellatedTopLeftHexagon();
+            if (leftHexagon.inWorld(world)) {
+                hexQueue.offer(leftHexagon);
+            }
+            Hexagon rightHexagon = hexagon.tessellatedTopRightHexagon();
+            if (rightHexagon.inWorld(world)) {
+                hexQueue.offer(rightHexagon);
+            }
+            if (hexagon.inWorld(world)) {
+                hexagon.addToWorld(world, randomTile());
+            }
+        }
 
         // draws the world to the screen
         renderer.renderFrame(world);
--- proj3/byow/lab12/HexWorldTest.java
+++ proj3/byow/lab12/HexWorldTest.java
@@ -1,6 +1,5 @@
 package byow.lab12;
 
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,87 +8,89 @@ import java.awt.*;
 public class HexWorldTest {
     @Test
     public void hexagonHeightTest() {
-        Assert.assertEquals(2, HexWorld.hexagonHeight(1));
-        Assert.assertEquals(4, HexWorld.hexagonHeight(2));
-        Assert.assertEquals(6, HexWorld.hexagonHeight(3));
-        Assert.assertEquals(8, HexWorld.hexagonHeight(4));
-        Assert.assertEquals(10, HexWorld.hexagonHeight(5));
+        Point lowerLeftPt = new Point(0, 0);
+        Assert.assertEquals(2, new HexWorld.Hexagon(lowerLeftPt, 1).height());
+        Assert.assertEquals(4, new HexWorld.Hexagon(lowerLeftPt, 2).height());
+        Assert.assertEquals(6, new HexWorld.Hexagon(lowerLeftPt, 3).height());
+        Assert.assertEquals(8, new HexWorld.Hexagon(lowerLeftPt, 4).height());
+        Assert.assertEquals(10, new HexWorld.Hexagon(lowerLeftPt, 5).height());
     }
 
     @Test
     public void hexagonWidthTest() {
-        Assert.assertEquals(1, HexWorld.hexagonWidth(1));
-        Assert.assertEquals(4, HexWorld.hexagonWidth(2));
-        Assert.assertEquals(7, HexWorld.hexagonWidth(3));
-        Assert.assertEquals(10, HexWorld.hexagonWidth(4));
-        Assert.assertEquals(13, HexWorld.hexagonWidth(5));
+        Point lowerLeftPt = new Point(0, 0);
+        Assert.assertEquals(1, new HexWorld.Hexagon(lowerLeftPt, 1).width());
+        Assert.assertEquals(4, new HexWorld.Hexagon(lowerLeftPt, 2).width());
+        Assert.assertEquals(7, new HexWorld.Hexagon(lowerLeftPt, 3).width());
+        Assert.assertEquals(10, new HexWorld.Hexagon(lowerLeftPt, 4).width());
+        Assert.assertEquals(13, new HexWorld.Hexagon(lowerLeftPt, 5).width());
     }
 
     @Test
-    public void hexagonStartXTest() {
+    public void hexagonVertStartXTest() {
         // Start point at (0, 0)
         Point startPt = new Point(0, 0);
-        // Test hexagon with side 1
-        int level1 = 1;
-        Assert.assertEquals(0, HexWorld.hexagonStartX(startPt, level1, 0));
-        Assert.assertEquals(0, HexWorld.hexagonStartX(startPt, level1, 1));
-        // Test hexagon with side 2
-        int level2 = 2;
-        Assert.assertEquals(1, HexWorld.hexagonStartX(startPt, level2, 0));
-        Assert.assertEquals(0, HexWorld.hexagonStartX(startPt, level2, 1));
-        Assert.assertEquals(0, HexWorld.hexagonStartX(startPt, level2, 2));
-        Assert.assertEquals(1, HexWorld.hexagonStartX(startPt, level2, 3));
-        // Test hexagon with side 3
-        int level3 = 3;
-        Assert.assertEquals(2, HexWorld.hexagonStartX(startPt, level3, 0));
-        Assert.assertEquals(1, HexWorld.hexagonStartX(startPt, level3, 1));
-        Assert.assertEquals(0, HexWorld.hexagonStartX(startPt, level3, 2));
-        Assert.assertEquals(0, HexWorld.hexagonStartX(startPt, level3, 3));
-        Assert.assertEquals(1, HexWorld.hexagonStartX(startPt, level3, 4));
-        Assert.assertEquals(2, HexWorld.hexagonStartX(startPt, level3, 5));
-        // Test hexagon with side 4
-        int level4 = 4;
-        Assert.assertEquals(3, HexWorld.hexagonStartX(startPt, level4, 0));
-        Assert.assertEquals(2, HexWorld.hexagonStartX(startPt, level4, 1));
-        Assert.assertEquals(1, HexWorld.hexagonStartX(startPt, level4, 2));
-        Assert.assertEquals(0, HexWorld.hexagonStartX(startPt, level4, 3));
-        Assert.assertEquals(0, HexWorld.hexagonStartX(startPt, level4, 4));
-        Assert.assertEquals(1, HexWorld.hexagonStartX(startPt, level4, 5));
-        Assert.assertEquals(2, HexWorld.hexagonStartX(startPt, level4, 6));
-        Assert.assertEquals(3, HexWorld.hexagonStartX(startPt, level4, 7));
+        // Test hexagon with size 1
+        HexWorld.Hexagon hex1 = new HexWorld.Hexagon(startPt, 1);
+        Assert.assertEquals(0, hex1.vertStartX(0));
+        Assert.assertEquals(0, hex1.vertStartX(1));
+        // Test hexagon with size 2
+        HexWorld.Hexagon hex2 = new HexWorld.Hexagon(startPt, 2);
+        Assert.assertEquals(1, hex2.vertStartX(0));
+        Assert.assertEquals(0, hex2.vertStartX(1));
+        Assert.assertEquals(0, hex2.vertStartX(2));
+        Assert.assertEquals(1, hex2.vertStartX(3));
+        // Test hexagon with size 3
+        HexWorld.Hexagon hex3 = new HexWorld.Hexagon(startPt, 3);
+        Assert.assertEquals(2, hex3.vertStartX(0));
+        Assert.assertEquals(1, hex3.vertStartX(1));
+        Assert.assertEquals(0, hex3.vertStartX(2));
+        Assert.assertEquals(0, hex3.vertStartX(3));
+        Assert.assertEquals(1, hex3.vertStartX(4));
+        Assert.assertEquals(2, hex3.vertStartX(5));
+        // Test hexagon with size 4
+        HexWorld.Hexagon hex4 = new HexWorld.Hexagon(startPt, 4);
+        Assert.assertEquals(3, hex4.vertStartX(0));
+        Assert.assertEquals(2, hex4.vertStartX(1));
+        Assert.assertEquals(1, hex4.vertStartX(2));
+        Assert.assertEquals(0, hex4.vertStartX(3));
+        Assert.assertEquals(0, hex4.vertStartX(4));
+        Assert.assertEquals(1, hex4.vertStartX(5));
+        Assert.assertEquals(2, hex4.vertStartX(6));
+        Assert.assertEquals(3, hex4.vertStartX(7));
     }
 
     @Test
-    public void hexagonEndPtTest() {
+    public void hexagonVertEndXTest() {
         // Start point at (0, 0)
         Point startPt = new Point(0, 0);
-        // Test hexagon with side 1
-        int level1 = 1;
-        Assert.assertEquals(0, HexWorld.hexagonEndX(startPt, level1, 0));
-        Assert.assertEquals(0, HexWorld.hexagonEndX(startPt, level1, 1));
-        // Test hexagon with side 2
-        int level2 = 2;
-        Assert.assertEquals(2, HexWorld.hexagonEndX(startPt, level2, 0));
-        Assert.assertEquals(3, HexWorld.hexagonEndX(startPt, level2, 1));
-        Assert.assertEquals(3, HexWorld.hexagonEndX(startPt, level2, 2));
-        Assert.assertEquals(2, HexWorld.hexagonEndX(startPt, level2, 3));
-        // Test hexagon with side 3
-        int level3 = 3;
-        Assert.assertEquals(4, HexWorld.hexagonEndX(startPt, level3, 0));
-        Assert.assertEquals(5, HexWorld.hexagonEndX(startPt, level3, 1));
-        Assert.assertEquals(6, HexWorld.hexagonEndX(startPt, level3, 2));
-        Assert.assertEquals(6, HexWorld.hexagonEndX(startPt, level3, 3));
-        Assert.assertEquals(5, HexWorld.hexagonEndX(startPt, level3, 4));
-        Assert.assertEquals(4, HexWorld.hexagonEndX(startPt, level3, 5));
-        // Test hexagon with side 4
-        int level4 = 4;
-        Assert.assertEquals(6, HexWorld.hexagonEndX(startPt, level4, 0));
-        Assert.assertEquals(7, HexWorld.hexagonEndX(startPt, level4, 1));
-        Assert.assertEquals(8, HexWorld.hexagonEndX(startPt, level4, 2));
-        Assert.assertEquals(9, HexWorld.hexagonEndX(startPt, level4, 3));
-        Assert.assertEquals(9, HexWorld.hexagonEndX(startPt, level4, 4));
-        Assert.assertEquals(8, HexWorld.hexagonEndX(startPt, level4, 5));
-        Assert.assertEquals(7, HexWorld.hexagonEndX(startPt, level4, 6));
-        Assert.assertEquals(6, HexWorld.hexagonEndX(startPt, level4, 7));
+        // Test hexagon with size 1
+        HexWorld.Hexagon hex1 = new HexWorld.Hexagon(startPt, 1);
+        Assert.assertEquals(0, hex1.vertEndX(0));
+        Assert.assertEquals(0, hex1.vertEndX(1));
+        // Test hexagon with size 2
+        HexWorld.Hexagon hex2 = new HexWorld.Hexagon(startPt, 2);
+        Assert.assertEquals(2, hex2.vertEndX(0));
+        Assert.assertEquals(3, hex2.vertEndX(1));
+        Assert.assertEquals(3, hex2.vertEndX(2));
+        Assert.assertEquals(2, hex2.vertEndX(3));
+        // Test hexagon with size 3
+        HexWorld.Hexagon hex3 = new HexWorld.Hexagon(startPt, 3);
+        Assert.assertEquals(4, hex3.vertEndX(0));
+        Assert.assertEquals(5, hex3.vertEndX(1));
+        Assert.assertEquals(6, hex3.vertEndX(2));
+        Assert.assertEquals(6, hex3.vertEndX(3));
+        Assert.assertEquals(5, hex3.vertEndX(4));
+        Assert.assertEquals(4, hex3.vertEndX(5));
+        // Test hexagon with size 4
+        HexWorld.Hexagon hex4 = new HexWorld.Hexagon(startPt, 4);
+        Assert.assertEquals(6, hex4.vertEndX(0));
+        Assert.assertEquals(7, hex4.vertEndX(1));
+        Assert.assertEquals(8, hex4.vertEndX(2));
+        Assert.assertEquals(9, hex4.vertEndX(3));
+        Assert.assertEquals(9, hex4.vertEndX(4));
+        Assert.assertEquals(8, hex4.vertEndX(5));
+        Assert.assertEquals(7, hex4.vertEndX(6));
+        Assert.assertEquals(6, hex4.vertEndX(7));
     }
 }
